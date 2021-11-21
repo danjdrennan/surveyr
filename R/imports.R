1ec990df774a79a50689bb21936251f27d567bb0
Daniel J. Drennan <danjdrennan@stat.tamu.edu>
2021-11-20 19:50:08 -0600

    uses stats in package
    The stats library provides excellent sd, var, cov, and cor functions.
    We will import those for use in this library rather than rewriting them.
    Some variance functions will still need to be written from scratch, but these ones are nice to use in some cases.

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,8 @@
 # imports.R
 #' @import dplyr
 #' @importFrom forcats as_factor
+#' @importFrom stats sd
+#' @importFrom stats var
+#' @importFrom stats cov
+#' @importFrom cor
 NULL
---
+++ tests/testthat/test-statsImports.R
@@ -0,0 +1,15 @@
+# We won't write better implementations of the L^2 functions than the ones in
+# stats.
+# Those packages already do excellent error handling.
+# Thus, we will import them as dependencies.
+# This decision could be changed in a future version of the package with minimal
+# impact on the user.
+test_that("var, sd, cov, cor are in NAMESPACE", {
+  x <- -1:1
+  expect_equal(var(x), 1.0)
+  expect_equal(sd(x), 1.0)
+
+  y <- -1:1
+  expect_equal(cov(x, y), 1.0)
+  expect_equal(cor(x, y), 1.0)
+})
