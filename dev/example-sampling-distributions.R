9b76eaf02e8cf29965f1bb079e4f38df6f18dbbf
Daniel J. Drennan <danjdrennan@stat.tamu.edu>
2021-11-22 08:43:32 -0600

    reworks script to use surveyr package

--- dev/example-sampling-distributions.R
+++ dev/example-sampling-distributions.R
@@ -3,53 +3,31 @@
 # and visualizing the estimators found from it.
 # Parts of this code will need to be imported into the package with tests
 # to prove correctness. They provide a good demo for the CLT when visualizing
-# sampling distributions
-library(tidyverse)
+# sampling distributions.
 
-N <- 30
-n <- 4
-
-mkSamplingDistribution <- function(n, N, random_dist, ...){
-  # The function generates all samples of size n from a population of size N
-  # using the combn function, which outputs all possible samples to an array.
-  # Large combinations can be used in the function, but it does not handle very
-  # large populations or combinations with very large sample sizes.
-  
-  # dist must be one of the random distributions
-  # The ellipsis allows for optional arguments to use with dist
-  # Create a population to sample from and generate statistics from
-  
-  # Generate the population data (using size N)
-  population_data <- random_dist(N, ...)
-  
-  # Create all SRS's of size n
-  samples <- combn(1:N, n, simplify = T)
-  
-  matrix(population_data[samples], ncol(samples), nrow(samples), byrow=T) %>%
-    rowMeans() -> sampling_dist
-  
-  return(list(
-    population = as.data.frame(population_data),
-    sampling_distribution = as.data.frame(sampling_dist)
-  ))
-}
-
-plot_population <- function(population){
-  ggplot(population) + 
-    geom_histogram(aes(x = population_data), color='gray', bins=20)
-}
-
-plot_samping_distribution <- function(sampling_dist, pop_mean){
-  ggplot(sampling_dist) + 
-    geom_histogram(aes(x = sampling_dist), color='gray', bins=20) +
-    geom_vline(xintercept = pop_mean, color="red")
-}
-
-sampling_dist <- mkSamplingDistribution(n, N, runif, min = 0, max = 100)
-
-sampling_dist$population %>% plot_population()
-
-pop_mean <- mean(sampling_dist$population$population_data)
-
-sampling_dist$sampling_distribution %>% 
-  plot_samping_distribution(pop_mean = pop_mean)
+# Now try the surveyr implementation
+library(surveyr)
+library(dplyr)
+library(ggplot2)
+N <- 20
+y <- rnorm(N, 30, 3)
+sampling_dist <- sampling_distribution(y, 6)
+df <- as_tibble(cbind(
+    "pop" = c(
+        rep("population", N),
+        rep("sampling_distribution", length(sampling_dist))
+    ),
+    "y" = c(y, sampling_dist)
+))
+df["y"] <- as.double(df$y)
+df
+ggplot(df, aes(x = y)) +
+    geom_histogram(aes(y=..density..), color="gray", fill="4e5c68") +
+    geom_density(color = "blue", size=1) +
+    facet_wrap(~pop) +
+    labs(
+        title="Example finite population",
+        x = "y",
+        y = "density"
+    ) +
+    theme_bw()
