9a668943f978fca3bc70584b0b274072b53f47da
Daniel J. Drennan <danjdrennan@stat.tamu.edu>
2021-11-21 21:42:19 -0600

    reformats indentation for tests

--- tests/testthat/test-dplyrImports.R
+++ tests/testthat/test-dplyrImports.R
@@ -1,8 +1,8 @@
 test_that("dplyr imports with pipe operator %>%", {
     # This test will only work if dplyr is imported into the software as expected
     # and if the pipe opertor %>% works as expected.
-  d <- as_tibble(cbind("a" = 1, "b" = 1:5)) %>%
-      summarize("mean_a" = mean(a), "mean_b" = mean(b))
-  expect_equal(d$mean_a, 1)
-  expect_equal(d$mean_b, 3)
+    d <- as_tibble(cbind("a" = 1, "b" = 1:5)) %>%
+        summarize("mean_a" = mean(a), "mean_b" = mean(b))
+    expect_equal(d$mean_a, 1)
+    expect_equal(d$mean_b, 3)
 })
--- tests/testthat/test-statsImports.R
+++ tests/testthat/test-statsImports.R
@@ -5,11 +5,11 @@
 # This decision could be changed in a future version of the package with minimal
 # impact on the user.
 test_that("var, sd, cov, cor are in NAMESPACE", {
-  x <- -1:1
-  expect_equal(var(x), 1.0)
-  expect_equal(sd(x), 1.0)
+    x <- -1:1
+    expect_equal(var(x), 1.0)
+    expect_equal(sd(x), 1.0)
 
-  y <- -1:1
-  expect_equal(cov(x, y), 1.0)
-  expect_equal(cor(x, y), 1.0)
+    y <- -1:1
+    expect_equal(cov(x, y), 1.0)
+    expect_equal(cor(x, y), 1.0)
 })
