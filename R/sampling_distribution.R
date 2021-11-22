b69a65a5cf6be3218f23f550d178cf1efb3f47cc
Daniel J. Drennan <danjdrennan@stat.tamu.edu>
2021-11-22 00:04:04 -0600

    adds documentation to sampling_distribution
    TODO:
     - Generalize to use means or proportions for input data

--- R/sampling_distribution.R
+++ R/sampling_distribution.R
@@ -1,3 +1,28 @@
+#' sampling_distribution
+#'
+#' @description
+#' Computes the sampling distribution of an estimator given a population,
+#' sample size, and statistic.
+#' The population must be a vector of numeric variables.
+#' The statistic must be the sample mean, sample total, or sample proportion.
+#' The function must store the entire sample as a matrix at some point, so it is
+#' bound on what samples can taken by choose(N, n).
+#'
+#' @param y
+#' A numeric vector of measurements on observations units.
+#' The population, N, will be computed as the length of y.
+#' @param n
+#' The size of samples to draw from y.
+#'
+#' @return
+#' The sampling distribution of the sample mean
+#' @export
+#'
+#' @examples
+#' y <- c(1, 1, 2, 2, 3, 3)
+#' n <- 2
+#' sampling_distribution(y, n)
+#' # 1.0 1.5 1.5 2.0 2.0 1.5 1.5 2.0 2.0 2.0 2.5 2.5 2.5 2.5 3.0
 sampling_distribution <- function(y, n){
     if(!(is.vector(y) && is.numeric(y))){
         stop("y must be a numeric vector")
